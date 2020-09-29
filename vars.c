8722f2427812130248385ea977e9d811f7967e29
ethan-shenker <eshenker10@stuy.edu>
2020-09-29 14:04:48 -0400

    updated snake_casing

--- vars.c
+++ vars.c
@@ -6,11 +6,11 @@ int main()
     printf("The value of integer is %d\n", integer);
     long long_num = 3;
     printf("The sum of long_num and integer is %ld\n", integer + long_num);
-    unsigned int unsignedInt = 4000000;
-    printf("The value of the unsigned initialized integer is %d\n", unsignedInt);
+    unsigned int unsigned_int = 4000000;
+    printf("The value of the unsigned initialized integer is %d\n", unsigned_int);
     char letter = 'a';
     char number = 127;
-    unsigned char unsChar = 255;
-    printf("letter: %c, number: %c, unsigned char: %c\n", letter, number, unsChar);
+    unsigned char uns_char = 255;
+    printf("letter: %c, number: %c, unsigned char: %c\n", letter, number, uns_char);
     return 0;
 }
